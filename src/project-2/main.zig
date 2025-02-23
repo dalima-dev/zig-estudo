6befff9631faeb025e3d94619b060652813746f2
Daniel <dalima.dev@gmail.com>
2025-02-23 18:23:00 -0300

    project 2: implements function to initialize buffer

--- src/project-2/main.zig
+++ src/project-2/main.zig
@@ -3,6 +3,15 @@ const config = @import("config.zig");
 const request = @import("request.zig");
 const stdout = std.io.getStdOut().writer();
 
+pub fn initialize_buffer() [1000]u8 {
+    var buffer: [1000]u8 = undefined;
+    for (0..buffer.len) |i| {
+        buffer[i] = 0;
+    }
+
+    return buffer;
+}
+
 pub fn main() !void {
     const socket = try config.Socket.init();
     try stdout.print("Server Addr: {any}\n", .{socket._address});
@@ -10,11 +19,7 @@ pub fn main() !void {
     var server = try socket._address.listen(.{});
     const connection = try server.accept();
 
-    var buffer: [1000]u8 = undefined;
-    for (0..buffer.len) |i| {
-        buffer[i] = 0;
-    }
-
+    var buffer = initialize_buffer();
     try request.read_request(connection, &buffer);
 
     try stdout.print("{s}", .{buffer});
