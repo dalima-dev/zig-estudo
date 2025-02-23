45f197addde05fca0c61a0977b732bf9d1b9a40a
Daniel <dalima.dev@gmail.com>
2025-02-23 17:17:05 -0300

    run server with connection

--- src/project-2/main.zig
+++ src/project-2/main.zig
@@ -1,5 +1,11 @@
 const std = @import("std");
+const config = @import("config.zig");
+const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
-    std.debug.print("Esta é o projeto 2.\n", .{});
+    const socket = try config.Socket.init();
+    try stdout.print("Server Addr: {any}\n", .{socket._address});
+    var server = try socket._address.listen(.{});
+    const connection = try server.accept();
+    _ = connection;
 }
