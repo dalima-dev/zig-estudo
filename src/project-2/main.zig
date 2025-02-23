98eda9aec6abe4bddb0d1e4988e4beab88d3b5e9
Daniel <dalima.dev@gmail.com>
2025-02-23 18:12:59 -0300

    project 2: read request and print it

--- src/project-2/main.zig
+++ src/project-2/main.zig
@@ -1,11 +1,21 @@
 const std = @import("std");
 const config = @import("config.zig");
+const request = @import("request.zig");
 const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
     const socket = try config.Socket.init();
     try stdout.print("Server Addr: {any}\n", .{socket._address});
+
     var server = try socket._address.listen(.{});
     const connection = try server.accept();
-    _ = connection;
+
+    var buffer: [1000]u8 = undefined;
+    for (0..buffer.len) |i| {
+        buffer[i] = 0;
+    }
+
+    try request.read_request(connection, &buffer);
+
+    try stdout.print("{s}", .{buffer});
 }
