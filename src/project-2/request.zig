d843ca1ca0dd4cca36229bb5c50ad4ac132b402e
Daniel <dalima.dev@gmail.com>
2025-02-23 21:11:17 -0300

    implements Method struct to request.zig

--- src/project-2/request.zig
+++ src/project-2/request.zig
@@ -1,5 +1,24 @@
 const std = @import("std");
 const Connection = std.net.Server.Connection;
+const Map = std.static_string_map.StaticStringMap;
+
+const MethodMap = Map(Method).initComptime(.{
+    .{ "GET", Method.GET },
+});
+
+pub const Method = enum {
+    GET,
+    pub fn init(text: []const u8) !Method {
+        return MethodMap.get(text).?;
+    }
+    pub fn is_supported(m: []const u8) bool {
+        const method = MethodMap.get(m);
+        if (method) |_| {
+            return true;
+        }
+        return false;
+    }
+};
 
 pub fn read_request(connection: Connection, buffer: []u8) !void {
     const reader = connection.stream.reader();
