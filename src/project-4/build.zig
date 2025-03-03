1d10de489672a62398f0ca5774165bf120cee2e2
Daniel Alves <dalima.dev@gmail.com>
2025-03-03 19:39:43 -0300

    add step to run project 4

--- src/project-4/build.zig
+++ src/project-4/build.zig
@@ -1,15 +1,23 @@
 const std = @import("std");
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
         .name = "image_filter",
-        .root_source_file = b.path("image_filter.zig"),
+        .root_source_file = b.path("main.zig"),
         .target = target,
         .optimize = optimize,
     });
+
     exe.linkLibC();
     // Link to libspng library:
     exe.linkSystemLibrary("spng");
     b.installArtifact(exe);
+
+    const run = b.addRunArtifact(exe);
+    run.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("project-4", "Executa projeto 4");
+    run_step.dependOn(&run.step);
 }
