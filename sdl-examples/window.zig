02da343cd36c7787c39ce66af73a0a749bb3adae
Daniel <dalima.dev@gmail.com>
2025-03-04 16:54:26 -0300

    window code cleanup

--- sdl-examples/window.zig
+++ sdl-examples/window.zig
@@ -1,51 +1,17 @@
 const std = @import("std");
 
 const c = @cImport({
-    @cDefine("SDL_DISABLE_OLD_NAMES", {});
     @cInclude("SDL3/SDL.h");
-    @cInclude("SDL3/SDL_revision.h");
-    @cDefine("SDL_MAIN_HANDLED", {});
     @cInclude("SDL3/SDL_main.h");
 });
 
 pub fn main() !void {
-    errdefer |err| if (err == error.SdlError) std.log.err("SDL error: {s}", .{c.SDL_GetError()});
-
-    std.log.debug("SDL build time version: {d}.{d}.{d}", .{
-        c.SDL_MAJOR_VERSION,
-        c.SDL_MINOR_VERSION,
-        c.SDL_MICRO_VERSION,
-    });
-
-    std.log.debug("SDL build time revision: {s}", .{c.SDL_REVISION});
-    {
-        const version = c.SDL_GetVersion();
-        std.log.debug("SDL runtime version: {d}.{d}.{d}", .{
-            c.SDL_VERSIONNUM_MAJOR(version),
-            c.SDL_VERSIONNUM_MINOR(version),
-            c.SDL_VERSIONNUM_MICRO(version),
-        });
-        const revision: [*:0]const u8 = c.SDL_GetRevision();
-        std.log.debug("SDL runtime revision: {s}", .{revision});
-    }
-
     c.SDL_SetMainReady();
 
     _ = c.SDL_SetAppMetadata("Window", "0.0.0", "sdl-examples.window");
 
     _ = c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO | c.SDL_INIT_GAMEPAD);
     defer c.SDL_Quit();
-
-    std.log.debug("SDL video drivers: {}", .{fmtSdlDrivers(
-        c.SDL_GetCurrentVideoDriver().?,
-        c.SDL_GetNumVideoDrivers(),
-        c.SDL_GetVideoDriver,
-    )});
-    std.log.debug("SDL audio drivers: {}", .{fmtSdlDrivers(
-        c.SDL_GetCurrentAudioDriver().?,
-        c.SDL_GetNumAudioDrivers(),
-        c.SDL_GetAudioDriver,
-    )});
 
     const window_w = 640;
     const window_h = 480;
@@ -54,7 +20,7 @@ pub fn main() !void {
     const window: *c.SDL_Window, const renderer: *c.SDL_Renderer = create_window_and_renderer: {
         var window: ?*c.SDL_Window = null;
         var renderer: ?*c.SDL_Renderer = null;
-        _ = c.SDL_CreateWindowAndRenderer("Speedbreaker", window_w, window_h, 0, &window, &renderer);
+        _ = c.SDL_CreateWindowAndRenderer("Window", window_w, window_h, 0, &window, &renderer);
         errdefer comptime unreachable;
 
         break :create_window_and_renderer .{ window.?, renderer.? };
@@ -62,63 +28,11 @@ pub fn main() !void {
     defer c.SDL_DestroyRenderer(renderer);
     defer c.SDL_DestroyWindow(window);
 
-    std.log.debug("SDL render drivers: {}", .{fmtSdlDrivers(
-        c.SDL_GetRendererName(renderer).?,
-        c.SDL_GetNumRenderDrivers(),
-        c.SDL_GetRenderDriver,
-    )});
-
     main_loop: while (true) {
         var event: c.SDL_Event = undefined;
         var timeoutMS: i32 = -1;
         while (c.SDL_WaitEventTimeout(&event, timeoutMS)) : (timeoutMS = 0) {
             if (event.type == c.SDL_EVENT_QUIT) break :main_loop;
-        }
-
-        _ = c.SDL_SetRenderDrawColor(renderer, 0x47, 0x5b, 0x8d, 0xff);
-        _ = c.SDL_RenderClear(renderer);
-
-        _ = c.SDL_SetRenderScale(renderer, 2, 2);
-        {
-            _ = c.SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
-        }
-        _ = c.SDL_SetRenderScale(renderer, 1, 1);
-
-        _ = c.SDL_RenderPresent(renderer);
-    }
-}
-
-fn fmtSdlDrivers(
-    current_driver: [*:0]const u8,
-    num_drivers: c_int,
-    getDriver: *const fn (c_int) callconv(.C) ?[*:0]const u8,
-) std.fmt.Formatter(formatSdlDrivers) {
-    return .{ .data = .{
-        .current_driver = current_driver,
-        .num_drivers = num_drivers,
-        .getDriver = getDriver,
-    } };
-}
-
-fn formatSdlDrivers(
-    context: struct {
-        current_driver: [*:0]const u8,
-        num_drivers: c_int,
-        getDriver: *const fn (c_int) callconv(.C) ?[*:0]const u8,
-    },
-    comptime _: []const u8,
-    _: std.fmt.FormatOptions,
-    writer: anytype,
-) !void {
-    var i: c_int = 0;
-    while (i < context.num_drivers) : (i += 1) {
-        if (i != 0) {
-            try writer.writeAll(", ");
-        }
-        const driver = context.getDriver(i).?;
-        try writer.writeAll(std.mem.span(driver));
-        if (std.mem.orderZ(u8, context.current_driver, driver) == .eq) {
-            try writer.writeAll(" (current)");
         }
     }
 }
