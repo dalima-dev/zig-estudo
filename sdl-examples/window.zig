ca366ffda29f01d42c8dcf7fee29f13f187b1ee3
Daniel Alves <dalima.dev@gmail.com>
2025-03-05 23:46:29 -0300

    refact window example

--- sdl-examples/window.zig
+++ sdl-examples/window.zig
@@ -5,34 +5,47 @@ const c = @cImport({
     @cInclude("SDL3/SDL_main.h");
 });
 
-pub fn main() !void {
+const WINDOW_WIDTH = 640;
+const WINDOW_HEIGHT = 480;
+
+pub fn createWindownAndRenderer() struct { *c.SDL_Window, *c.SDL_Renderer } {
     c.SDL_SetMainReady();
 
     _ = c.SDL_SetAppMetadata("Window", "0.0.0", "sdl-examples.window");
-
     _ = c.SDL_Init(c.SDL_INIT_VIDEO);
-    defer c.SDL_Quit();
-
-    const window_w = 640;
-    const window_h = 480;
 
     const window: *c.SDL_Window, const renderer: *c.SDL_Renderer = create_window_and_renderer: {
         var window: ?*c.SDL_Window = null;
         var renderer: ?*c.SDL_Renderer = null;
-        _ = c.SDL_CreateWindowAndRenderer("Window", window_w, window_h, 0, &window, &renderer);
+        _ = c.SDL_CreateWindowAndRenderer("Window", WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer);
         errdefer comptime unreachable;
 
         break :create_window_and_renderer .{ window.?, renderer.? };
     };
+
+    return .{ window, renderer };
+}
+
+pub fn render(renderer: ?*c.SDL_Renderer) void {
+    _ = c.SDL_SetRenderDrawColor(renderer, 33, 33, 33, c.SDL_ALPHA_OPAQUE);
+    _ = c.SDL_RenderClear(renderer);
+
+    _ = c.SDL_RenderPresent(renderer);
+}
+
+pub fn main() !void {
+    const window: *c.SDL_Window, const renderer: *c.SDL_Renderer = createWindownAndRenderer();
+    defer c.SDL_Quit();
     defer c.SDL_DestroyRenderer(renderer);
     defer c.SDL_DestroyWindow(window);
 
     main_loop: while (true) {
         var event: c.SDL_Event = undefined;
-        var timeoutMS: i32 = -1;
 
-        while (c.SDL_WaitEventTimeout(&event, timeoutMS)) : (timeoutMS = 0) {
+        while (c.SDL_PollEvent(&event)) {
             if (event.type == c.SDL_EVENT_QUIT) break :main_loop;
         }
+
+        render(renderer);
     }
 }
