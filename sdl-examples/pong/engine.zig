b9ffbae84663562bbdbecd4808d9867dd4dddd89
Daniel Alves <dalima.dev@gmail.com>
2025-03-07 01:50:44 -0300

    refact code into engine and objects modules

---
+++ sdl-examples/pong/engine.zig
@@ -0,0 +1,70 @@
+const c = @cImport({
+    @cInclude("SDL3/SDL.h");
+    @cInclude("SDL3/SDL_main.h");
+});
+
+const objects = @import("objects.zig");
+const Ball = objects.Ball;
+
+pub const WINDOW_WIDTH = 640;
+pub const WINDOW_HEIGHT = 480;
+
+pub var last_time: u64 = 0;
+pub var current_time: u64 = 0;
+pub var elapsed_time: f32 = 0;
+
+const BALL_SIZE = objects.BALL_SIZE;
+pub var ball: Ball = undefined;
+
+pub fn handleBallCollisionWithWall() void {
+    const ball_position_x = ball.object.position.x;
+    const ball_position_y = ball.object.position.y;
+
+    const left_collision = ball_position_x < 0;
+    const right_collision = ball_position_x > WINDOW_WIDTH - BALL_SIZE;
+    const top_collision = ball_position_y < 0;
+    const bottom_collision = ball_position_y > WINDOW_HEIGHT - BALL_SIZE;
+
+    if (left_collision) {
+        ball.setVelocity(-ball.object.velocity.x, ball.object.velocity.y);
+        ball.setPosition(0, ball_position_y);
+    }
+
+    if (right_collision) {
+        ball.setVelocity(-ball.object.velocity.x, ball.object.velocity.y);
+        ball.setPosition(WINDOW_WIDTH - BALL_SIZE, ball_position_y);
+    }
+
+    if (top_collision) {
+        ball.setVelocity(ball.object.velocity.x, -ball.object.velocity.y);
+        ball.setPosition(ball_position_x, 0);
+    }
+
+    if (bottom_collision) {
+        ball.setVelocity(ball.object.velocity.x, -ball.object.velocity.y);
+        ball.setPosition(ball_position_x, WINDOW_HEIGHT - BALL_SIZE);
+    }
+}
+
+pub fn updateBallPositionByElapsedTime() void {
+    const ball_position_x = ball.object.position.x + ball.object.velocity.x * elapsed_time;
+    const ball_position_y = ball.object.position.y + ball.object.velocity.y * elapsed_time;
+    ball.setPosition(ball_position_x, ball_position_y);
+}
+
+pub fn updateBallState() void {
+    updateBallPositionByElapsedTime();
+    handleBallCollisionWithWall();
+}
+
+pub fn initialize() void {
+    ball = Ball.init((WINDOW_WIDTH - BALL_SIZE) / 2, (WINDOW_HEIGHT - BALL_SIZE) / 2, 300, 300);
+}
+
+pub fn update() void {
+    updateBallState();
+}
+
+pub fn draw(renderer: ?*c.SDL_Renderer) void {
+    ball.draw(renderer);
+}
--- sdl-examples/pong/main.zig
+++ sdl-examples/pong/main.zig
@@ -5,16 +5,13 @@ const c = @cImport({
     @cInclude("SDL3/SDL_main.h");
 });
 
-const WINDOW_WIDTH = 640;
-const WINDOW_HEIGHT = 480;
+const engine = @import("engine.zig");
+const WINDOW_WIDTH = engine.WINDOW_WIDTH;
+const WINDOW_HEIGHT = engine.WINDOW_HEIGHT;
 
-var last_time: u64 = 0;
-var current_time: u64 = 0;
-var elapsed_time: f32 = 0;
-
-// declare objects here...
-const BALL_SIZE = 10;
-var ball: Ball = undefined;
+var last_time_ptr: *u64 = &engine.last_time;
+var current_time_ptr: *u64 = &engine.current_time;
+var elapsed_time_ptr: *f32 = &engine.elapsed_time;
 
 pub fn createWindownAndRenderer() struct { *c.SDL_Window, *c.SDL_Renderer } {
     c.SDL_SetMainReady();
@@ -34,70 +31,11 @@ pub fn createWindownAndRenderer() struct { *c.SDL_Window, *c.SDL_Renderer } {
     return .{ window, renderer };
 }
 
-fn initializeAppState() void {
-    last_time = c.SDL_GetTicks();
-
-    // initialize objects here...
-    ball = Ball.init((WINDOW_WIDTH - BALL_SIZE) / 2, (WINDOW_HEIGHT - BALL_SIZE) / 2, 300, 300);
-}
-
-fn handleBallCollisionWithWall() void {
-    const ball_position_x = ball.object.position.x;
-    const ball_position_y = ball.object.position.y;
-
-    const left_collision = ball_position_x < 0;
-    const right_collision = ball_position_x > WINDOW_WIDTH - BALL_SIZE;
-    const top_collision = ball_position_y < 0;
-    const bottom_collision = ball_position_y > WINDOW_HEIGHT - BALL_SIZE;
-
-    if (left_collision) {
-        ball.setVelocity(-ball.object.velocity.x, ball.object.velocity.y);
-        ball.setPosition(0, ball_position_y);
-    }
-
-    if (right_collision) {
-        ball.setVelocity(-ball.object.velocity.x, ball.object.velocity.y);
-        ball.setPosition(WINDOW_WIDTH - BALL_SIZE, ball_position_y);
-    }
-
-    if (top_collision) {
-        ball.setVelocity(ball.object.velocity.x, -ball.object.velocity.y);
-        ball.setPosition(ball_position_x, 0);
-    }
-
-    if (bottom_collision) {
-        ball.setVelocity(ball.object.velocity.x, -ball.object.velocity.y);
-        ball.setPosition(ball_position_x, WINDOW_HEIGHT - BALL_SIZE);
-    }
-}
-
-fn updateBallPositionByElapsedTime() void {
-    const ball_position_x = ball.object.position.x + ball.object.velocity.x * elapsed_time;
-    const ball_position_y = ball.object.position.y + ball.object.velocity.y * elapsed_time;
-    ball.setPosition(ball_position_x, ball_position_y);
-}
-
-fn updateBallState() void {
-    updateBallPositionByElapsedTime();
-    handleBallCollisionWithWall();
-}
-
-fn updateAppState() void {
-    current_time = c.SDL_GetTicks();
-    elapsed_time = @as(f32, @floatFromInt(current_time - last_time)) / 1000;
-
-    // update objects here...
-    updateBallState();
-
-    last_time = c.SDL_GetTicks();
-}
-
 fn render(renderer: ?*c.SDL_Renderer) void {
     _ = c.SDL_SetRenderDrawColor(renderer, 0, 0, 0, c.SDL_ALPHA_OPAQUE);
     _ = c.SDL_RenderClear(renderer);
 
-    // render objects here...
-    ball.draw(renderer);
+    engine.draw(renderer);
 
     _ = c.SDL_RenderPresent(renderer);
 }
@@ -108,7 +46,8 @@ pub fn main() !void {
     defer c.SDL_DestroyRenderer(renderer);
     defer c.SDL_DestroyWindow(window);
 
-    initializeAppState();
+    last_time_ptr.* = c.SDL_GetTicks();
+    engine.initialize();
 
     main_loop: while (true) {
         var event: c.SDL_Event = undefined;
@@ -118,61 +57,13 @@ pub fn main() !void {
             if (event.type == c.SDL_EVENT_QUIT) break :main_loop;
         }
 
-        updateAppState();
+        {
+            current_time_ptr.* = c.SDL_GetTicks();
+            elapsed_time_ptr.* = @as(f32, @floatFromInt(current_time_ptr.* - last_time_ptr.*)) / 1000;
+            engine.update();
+            last_time_ptr.* = c.SDL_GetTicks();
+        }
+
         render(renderer);
     }
 }
-
-const Position = struct {
-    x: f32,
-    y: f32,
-};
-
-const Velocity = struct {
-    x: f32,
-    y: f32,
-};
-
-const Object = struct {
-    position: Position,
-    velocity: Velocity,
-};
-
-const Ball = struct {
-    object: Object,
-    shape: c.SDL_FRect,
-
-    pub fn init(pos_x: f32, pos_y: f32, vel_x: f32, vel_y: f32) Ball {
-        return Ball{
-            .object = .{
-                .position = .{ .x = pos_x, .y = pos_y },
-                .velocity = .{ .x = vel_x, .y = vel_y },
-            },
-            .shape = c.SDL_FRect{
-                .x = pos_x,
-                .y = pos_y,
-                .w = BALL_SIZE,
-                .h = BALL_SIZE,
-            },
-        };
-    }
-
-    pub fn setPosition(self: *Ball, x: f32, y: f32) void {
-        self.object.position.x = x;
-        self.object.position.y = y;
-
-        self.shape.x = x;
-        self.shape.y = y;
-    }
-
-    pub fn setVelocity(self: *Ball, x: f32, y: f32) void {
-        self.object.velocity.x = x;
-        self.object.velocity.y = y;
-    }
-
-    pub fn draw(self: *Ball, renderer: ?*c.SDL_Renderer) void {
-        const rect = self.shape;
-        _ = c.SDL_SetRenderDrawColor(renderer, 255, 255, 255, c.SDL_ALPHA_OPAQUE);
-        _ = c.SDL_RenderFillRect(renderer, &rect);
-    }
-};
